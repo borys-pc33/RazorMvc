8a12229ee47d65b02c450796584d600a3dea7d0d
borys_pc33 <borys.lebeda@principal33.com>
2021-03-25 07:58:40 +0200

    Use model to display members

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,16 +11,18 @@ namespace RazorMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly InternshipClass _internshipClass;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
+            _internshipClass = new InternshipClass();
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_internshipClass);
         }
 
         public IActionResult Privacy()
---
+++ Models/InternshipClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RazorMvc.Models
+{
+    public class InternshipClass
+    {
+        private List<string> _members;
+
+        public InternshipClass()
+        {
+            _members = new List<string>
+            {
+                "Borys",
+                "Liova",
+                "Orest",
+            };
+        }
+
+        public IList<string> Members
+        {
+            get { return _members; }
+        }
+    }
+}
