e25bedb64a37c438dddbb7e70c693dbeb046687c
borys_pc33 <borys.lebeda@principal33.com>
2021-03-25 07:47:11 +0200

    Implement internship class list
    Use JavaScript to implement logic

---
+++ wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#add").click(function () {
+        var newcomerName = $("#newcomer").val();
+
+        // Remember string interpolation
+        $("#list").append(`<li>${newcomerName}</li>`);
+
+        $("#newcomer").val("");
+    })
+});
