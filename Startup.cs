e22e48cc519870cc6441b85ad855832a8803a445
borysl <borys.lebeda@gmail.com>
2021-04-11 17:48:32 +0300

    Remove identity

--- Startup.cs
+++ Startup.cs
@@ -24,9 +24,6 @@ namespace RazorMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<InternDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
