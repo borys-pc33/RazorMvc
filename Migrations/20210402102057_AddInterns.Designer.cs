acdb68a505712197e3dffcf7b2af4df475bdc7ae
borysl <borys.lebeda@gmail.com>
2021-04-15 08:43:52 +0300

    Fix problem with the annotation - it doesn't exists anymore

--- Migrations/20210402102057_AddInterns.Designer.cs
+++ Migrations/20210402102057_AddInterns.Designer.cs
@@ -18,15 +18,13 @@ namespace RazorMvc.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.4")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion", "5.0.4");
 
             modelBuilder.Entity("RazorMvc.Models.Intern", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                        .HasColumnType("int");
 
                     b.Property<DateTime>("DateOfJoin")
                         .HasColumnType("datetime2");
